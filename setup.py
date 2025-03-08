5a11451a5360b3ab50c3a8613ef25599550b596a
Ihebski <ihebski@users.noreply.github.com>
2025-03-08 17:43:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
         install_requires = f.read().splitlines()
 
 setup(name='default-creds',
-    version='0.4',
+    version='0.5.3',
     description='One place for all the default credentials to assist pentesters during an engagement, this document has several products default login/password gathered from multiple sources.',
     url='https://github.com/ihebski/DefaultCreds-cheat-sheet.git',
     install_requires=install_requires,
